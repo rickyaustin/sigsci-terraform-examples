3e7bf9f5835b6cc4394205f280752e918c156cd1
rickyaustin <82475896+rickyaustin@users.noreply.github.com>
2022-11-07 16:14:59 -0500

    Update alerts.tf

--- alerts.tf
+++ alerts.tf
@@ -1,16 +1,5 @@
-resource "sigsci_site_alert" "test" {
-  site_short_name    = "gcp_lab"
-  tag_name           = sigsci_site_signal_tag.testalert.id
-  long_name          = "Test Alert"
-  interval           = 1
-  threshold          = 10
-  enabled            = true
-  action             = "flagged"
-  skip_notifications = true
-}
-
 resource "sigsci_site_alert" "XSS" {
-  site_short_name    = "gcp_lab"
+  site_short_name    = var.sigsci_site
   tag_name           = "XSS"
   long_name          = "XSS Threshold"
   interval           = 1
@@ -21,7 +10,7 @@ resource "sigsci_site_alert" "XSS" {
 }
 
 resource "sigsci_site_alert" "SQLI" {
-  site_short_name    = "gcp_lab"
+  site_short_name    = var.sigsci_site
   tag_name           = "SQLI"
   long_name          = "SQLI Threshold"
   interval           = 1
@@ -32,9 +21,42 @@ resource "sigsci_site_alert" "SQLI" {
 }
 
 resource "sigsci_site_alert" "CMDEXE" {
-  site_short_name    = "gcp_lab"
+  site_short_name    = var.sigsci_site
   tag_name           = "CMDEXE"
   long_name          = "CMD Threshold"
+  interval           = 1
+  threshold          = 10
+  enabled            = true
+  action             = "flagged"
+  skip_notifications = true
+}
+
+resource "sigsci_site_alert" "TRAVERSAL" {
+  site_short_name    = var.sigsci_site
+  tag_name           = "TRAVERSAL"
+  long_name          = "Traversal Threshold"
+  interval           = 1
+  threshold          = 10
+  enabled            = true
+  action             = "flagged"
+  skip_notifications = true
+}
+
+resource "sigsci_site_alert" "BACKDOOR" {
+  site_short_name    = var.sigsci_site
+  tag_name           = "BACKDOOR"
+  long_name          = "Backdoor Threshold"
+  interval           = 1
+  threshold          = 10
+  enabled            = true
+  action             = "flagged"
+  skip_notifications = true
+}
+
+resource "sigsci_site_alert" "USERAGENT" {
+  site_short_name    = var.sigsci_site
+  tag_name           = "USERAGENT"
+  long_name          = "Attack Tooling Threshold"
   interval           = 1
   threshold          = 10
   enabled            = true
